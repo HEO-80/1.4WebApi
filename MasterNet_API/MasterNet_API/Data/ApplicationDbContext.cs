46c22778d80b00f6e6ccc3dacaf855ed2a69c806
Hector Oviedo <hectoroviedo@social11.es>
2022-04-08 11:03:41 +0200

    UP Exercise

---
+++ MasterNet_API/MasterNet_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using MasterNet_API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MasterNet_API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+
+
+        }
+
+        public DbSet<Post> posts  { get; set; }
+
+
+    }
+}
